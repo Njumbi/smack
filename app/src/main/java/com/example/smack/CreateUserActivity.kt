c14aace950ddb0e5a384393f2d293e2aff4dba3a
toni <wambuinjumbi@gmail.com>
2021-01-04 13:54:24 +0300

    Create sign up Activity Page

---
+++ app/src/main/java/com/example/smack/CreateUserActivity.kt
@@ -0,0 +1,22 @@
+package com.example.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creat_user)
+    }
+    fun generateUserAvatarClicked(view: View){
+
+    }
+    fun generateBackgroundColorBtnClicked(view: View){
+
+    }
+
+    fun createUserBtnClicked(view: View){
+
+    }
+}
--- app/src/main/java/com/example/smack/LoginActivity.kt
+++ app/src/main/java/com/example/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,11 +11,13 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
     }
 
-    fun loginLoginBtnClicked(view: View){
+    fun loginLoginBtnClicked(view: View) {
 
     }
 
-    fun loginCreateUserBtnClicked(view: View){
+    fun loginCreateUserBtnClicked(view: View) {
 
+        val createUser = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUser)
     }
 }
